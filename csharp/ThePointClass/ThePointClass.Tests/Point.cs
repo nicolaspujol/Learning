47634a3b95e31769c55ca57f221f83be5a9baeae
Nicolas PUJOL <nicolas.pujol31@gmail.com>
2017-01-20 17:59:20 +0000

    Update unit tests

--- csharp/ThePointClass/ThePointClass.Tests/Point.cs
+++ csharp/ThePointClass/ThePointClass.Tests/Point.cs
@@ -7,13 +7,24 @@ namespace ThePointClass.Tests
     public class PointTests
     {
         [TestMethod]
-        public void Distance()
+        public void DistanceAtOrigin()
         {
             Point point = new Point(0, 0);
             Assert.AreEqual(0, point.Distance());
+        }
 
-            Point point2 = new Point(1, 2);
-            Assert.AreEqual(2.23, point2.Distance(), 0.01);
+        [TestMethod]
+        public void DistanceAtPositivePoint()
+        {
+            Point point = new Point(1, 2);
+            Assert.AreEqual(2.23, point.Distance(), 0.01);
+        }
+
+        [TestMethod]
+        public void DistanceAtNegativePoint()
+        {
+            Point point = new Point(-3, -4);
+            Assert.AreEqual(5, point.Distance(), 0.01);
         }
     }
 }
