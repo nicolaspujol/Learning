805c5f79ebd7a343672d9718bfe2f38e67198aaf
Nicolas PUJOL <nicolas.pujol31@gmail.com>
2017-01-20 17:35:41 +0000

    the book class

---
+++ csharp/TheBookClass/TheBookClass/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBookClass
+{
+    class Book
+    {
+        private string _title;
+        private string _author;
+        private string _price;
+
+        public Book(string title, string author, string price) {
+            this._title = title;
+            this._author = author;
+            this._price = price;
+        }
+
+        public void DisplayBookData() {
+            Console.WriteLine($"Title: {_title}, Author: {_author}, Price: {_price}");
+        }
+    }
+}
---
+++ csharp/TheBookClass/TheBookClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBookClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++) {
+                Book book = AddBook();
+                book.DisplayBookData();
+            }
+        }
+
+        private static Book AddBook()
+        {
+            string bookTitle = getBookData("Title");
+            string bookAuthor = getBookData("Author");
+            string bookPrice = getBookData("Price");
+
+            return new Book(bookTitle, bookAuthor, bookPrice);
+        }
+
+        private static string getBookData(string expectedData)
+        {
+            Console.WriteLine($"{expectedData}:");
+            return Console.ReadLine();
+        }
+    }
+}
