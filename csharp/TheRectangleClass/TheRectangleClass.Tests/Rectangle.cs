7286aedd8bfd17abf4a41227ca590854b329d691
Nicolas PUJOL <nicolas.pujol31@gmail.com>
2017-01-23 10:54:37 +0000

    the rectangle class

---
+++ csharp/TheRectangleClass/TheRectangleClass.Tests/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheRectangleClass.Tests
+{
+    [TestClass]
+    public class RectangleTests
+    {
+        [TestMethod]
+        public void Perimeter()
+        {
+            Rectangle rect = new Rectangle(3, 4);
+            Assert.AreEqual(14, rect.Perimeter());
+        }
+        [TestMethod]
+        public void Area()
+        {
+            Rectangle rect = new Rectangle(3, 4);
+            Assert.AreEqual(12, rect.Area());
+        }
+        [TestMethod]
+        public void IsSquareTrue()
+        {
+            Rectangle rect = new Rectangle(3, 3);
+            Assert.AreEqual(true, rect.IsSquare());
+        }
+        [TestMethod]
+        public void IsSquareFalse()
+        {
+            Rectangle rect = new Rectangle(3, 4);
+            Assert.AreEqual(false, rect.IsSquare());
+        }
+        [TestMethod]
+        public void DisplayRectangleData()
+        {
+            Rectangle rect = new Rectangle(4, 5);
+            Assert.AreEqual("Length: 4\nWidth: 5\nPerimeter: 18\nArea: 20\nIs a square: False", rect.DisplayRectangleData());
+        }
+    }
+}
---
+++ csharp/TheRectangleClass/TheRectangleClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheRectangleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("Rectangle " + i + ":");
+                Rectangle rectangle = AddRectangle();
+                Console.WriteLine(rectangle.DisplayRectangleData());
+            }
+            Console.ReadKey();
+        }
+        private static Rectangle AddRectangle()
+        {
+            int rectangleLength = Int32.Parse(GetRectangleData("Length"));
+            int rectangleWidth = Int32.Parse(GetRectangleData("Width"));
+
+            return new Rectangle(rectangleLength, rectangleWidth);
+        }
+
+        private static string GetRectangleData(string expectedData)
+        {
+            Console.WriteLine($"{expectedData}?");
+            return Console.ReadLine();
+        }
+    }
+}
