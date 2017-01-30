23a9ec05fedd9bcece14ff7aee08ff2716b73231
Nicolas PUJOL <nicolas.pujol31@gmail.com>
2017-01-30 16:08:36 +0000

    inheritance accounts

---
+++ csharp/TheAccountClassInheritance/TheAccountClassInheritance.Unit.Tests/Accounts/PayingAccountTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheAccountClassInheritance.Unit.Tests.Accounts
+{
+    [TestClass]
+    public class PayingAccountTests
+    {
+        [TestMethod]
+        public void CreateAccount_WithoutParams_ShouldSetBalanceToZero()
+        {
+            PayingAccount account = new PayingAccount(13);
+            Assert.AreEqual(13, account.Balance);
+        }
+        [TestMethod]
+        public void Credit_WithAmount_ShouldIncreaseBalanceAndApplyFees()
+        {
+            PayingAccount account = new PayingAccount(999);
+            account.Credit(1000);
+
+            Assert.AreEqual(1994, account.Balance);
+        }
+        [TestMethod]
+        public void Debit_WithAmount_ShouldDecreaseBalanceAndApplyFees()
+        {
+            PayingAccount account = new PayingAccount(1000);
+            account.Debit(999);
+
+            Assert.AreEqual(-4, account.Balance);
+        }
+    }
+}
---
+++ csharp/TheAccountClassInheritance/TheAccountClassInheritance/Accounts/PayingAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAccountClassInheritance
+{
+    public class PayingAccount : Account
+    {
+        private const int _fee = 5;
+
+        public PayingAccount() : base() { }
+        public PayingAccount(double balance) : base(balance) { }
+
+        public new void Credit(double amount)
+        {
+            _balance += amount - 5;
+        }
+        public new void Debit(double amount)
+        {
+            _balance -= amount + 5;
+        }
+    }
+}
