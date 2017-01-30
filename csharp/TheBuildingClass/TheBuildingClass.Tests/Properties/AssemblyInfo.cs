2eedcc419420db7a7daa312934a3577b8081e59d
Nicolas PUJOL <nicolas.pujol31@gmail.com>
2017-01-30 14:12:48 +0000

    rename UnitTestProject1

--- csharp/TheBuildingClass/TheBuildingClass.Tests/Properties/AssemblyInfo.cs
+++ csharp/TheBuildingClass/TheBuildingClass.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f0da345e-3388-442a-a62d-3d76c6d7ff52")]
+[assembly: Guid("bc97ed0f-83d4-45d2-a0da-e9399640dd54")]
 
 // Version information for an assembly consists of the following four values:
 //
