ca7d1e672e9be48273321af5dd6e3c3ea7793154
Nicolas PUJOL <nicolas.pujol31@gmail.com>
2017-01-30 11:37:01 +0000

    Building class

---
+++ csharp/TheBuildingClass/UnitTestProject1/Building.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheBuildingClass;
+
+namespace TheBuildingClass.Tests
+{
+    [TestClass]
+    public class BuildingTests
+    {
+        [TestMethod]
+        public void BuildingToStringNoParamConstructor()
+        {
+            Building building = new Building();
+            Assert.AreEqual("Address: ", building.ToString());
+        }
+
+        [TestMethod]
+        public void BuildingToStringAdressConstructor()
+        {
+            Building building = new Building("London");
+            Assert.AreEqual("Address: London", building.ToString());
+        }
+    }
+}
---
+++ csharp/TheBuildingClass/UnitTestProject1/House.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheBuildingClass;
+
+namespace TheBuildingClass.Tests
+{
+    [TestClass]
+    public class HouseTests
+    {
+        [TestMethod]
+        public void HouseToStringNoParamConstructor()
+        {
+            House house = new House();
+            Assert.AreEqual("Address: , Rooms: 0", house.ToString());
+        }
+
+        [TestMethod]
+        public void HouseToStringAdressConstructor()
+        {
+            House house = new House("London", 2);
+            Assert.AreEqual("Address: London, Rooms: 2", house.ToString());
+        }
+    }
+}
